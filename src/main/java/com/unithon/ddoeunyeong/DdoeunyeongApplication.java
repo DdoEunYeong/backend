387c2991425b241684cc1d8214f6b9f7091c4ef6
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-08-07 19:53:10 +0900

    init : 초기세팅

---
+++ src/main/java/com/unithon/ddoeunyeong/DdoeunyeongApplication.java
@@ -0,0 +1,13 @@
+package com.unithon.ddoeunyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdoeunyeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdoeunyeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unithon/ddoeunyeong/DdoeunyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unithon.ddoeunyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdoeunyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
