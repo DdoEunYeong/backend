cdf3b732183687f3536bcdbef98b835442f55c6a
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-08-10 21:23:19 +0900

    feat : 제미나이로 배경 제거 추기 #14

---
+++ src/main/java/com/unithon/ddoeunyeong/infra/gemini/dto/GeminiResponse.java
@@ -0,0 +1,4 @@
+package com.unithon.ddoeunyeong.infra.gemini.dto;
+
+public record GeminiResponse(String imageUrl) {
+}
---
+++ src/main/java/com/unithon/ddoeunyeong/infra/gemini/dto/ImageEditRequest.java
@@ -0,0 +1,6 @@
+package com.unithon.ddoeunyeong.infra.gemini.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public record ImageEditRequest(Long childId) {
+}
